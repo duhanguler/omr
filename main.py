ec5e32f067cce39278a6de2f13b295131caa579a
duhanguler <45712664+duhanguler@users.noreply.github.com>
2023-06-10 00:04:05 +0300

    Core

---
+++ main.py
@@ -0,0 +1,86 @@
+"""
+
+ Optical Mark Recognition
+
+ Author: Duhan Guler, Eray Ergulec, Elif Baykal
+ Github: https://github.com/duhanguler
+
+"""
+
+import argparse
+from pathlib import Path
+
+from src.entry import entry_point
+from src.logger import logger
+
+
+def parse_args():
+    # construct the argument parse and parse the arguments
+    argparser = argparse.ArgumentParser()
+
+    argparser.add_argument(
+        "-i",
+        "--inputDir",
+        default=["inputs"],
+        # https://docs.python.org/3/library/argparse.html#nargs
+        nargs="*",
+        required=False,
+        type=str,
+        dest="input_paths",
+        help="Specify an input directory.",
+    )
+
+    argparser.add_argument(
+        "-o",
+        "--outputDir",
+        default="outputs",
+        required=False,
+        dest="output_dir",
+        help="Specify an output directory.",
+    )
+
+    argparser.add_argument(
+        "-a",
+        "--autoAlign",
+        required=False,
+        dest="autoAlign",
+        action="store_true",
+        help="(experimental) Enables automatic template alignment - \
+        use if the scans show slight misalignments.",
+    )
+
+    argparser.add_argument(
+        "-l",
+        "--setLayout",
+        required=False,
+        dest="setLayout",
+        action="store_true",
+        help="Set up OMR template layout - modify your json file and \
+        run again until the template is set.",
+    )
+
+    (
+        args,
+        unknown,
+    ) = argparser.parse_known_args()
+
+    args = vars(args)
+
+    if len(unknown) > 0:
+        logger.warning(f"\nError: Unknown arguments: {unknown}", unknown)
+        argparser.print_help()
+        exit(11)
+    return args
+
+
+def entry_point_for_args(args):
+    for root in args["input_paths"]:
+        entry_point(
+            Path(root),
+            args,
+        )
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    entry_point_for_args(args)
